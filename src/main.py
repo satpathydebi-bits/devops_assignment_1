d015f65e7b209fe2cb3333ca8274236039f726ee
satpathydebi <satpathydebi@gmail.com>
2024-05-01 17:38:32 +0530

    Add src and tests directories with raw code files

---
+++ src/main.py
@@ -0,0 +1,7 @@
+# File: src/main.py
+
+def main():
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    main()
---
+++ test/test_main.py
@@ -0,0 +1,6 @@
+# File: tests/test_main.py
+
+import devops_assignment_1.test.test_main as test_main  # Assuming main.py is in the src directory
+
+def test_main():
+    assert test_main.test_main() == "Hello, world!"
